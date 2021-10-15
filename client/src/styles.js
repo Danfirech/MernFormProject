6d0efce37ae1a24a2279c98816b4f0beb43568b8
Dan Possehl <danielbpossehl@gmail.com>
2021-10-15 11:13:12 -0600

    added Material UI components

---
+++ client/src/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core";
+
+export default makestyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 0",
+    display: "flex",
+    flexDirection: "column",
+    justifycontent: "center",
+    alignItems: "center",
+  },
+}));
